47519d6bea5c5d7bcb42845423f6783d11c8e6c7
abdellah <abdellahedaoudi80@gmail.com>
2024-11-15 16:31:00 +0100

    Body and Query and Param

--- src/Users/user.controller.ts
+++ src/Users/user.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -10,9 +10,23 @@ export class UserController {
   //   return this.userService.findAll()
   // }
 
+  // Body
   @Get()
-  getUser(){
-    return this.userService.findUser()
+  getUser(@Body() body:any){
+    console.log(body);
+    // return this.userService.findUser()
+  }
+  // Param
+  @Get(":id")
+  getUser1(@Param("id") param:any){
+    console.log(param);
+    // return this.userService.findUser()
+  }
+  // Query
+  @Post()
+  getUser2(@Query() query:any){
+    console.log(query);
+    // return this.userService.findUser()
   }
   
 }
