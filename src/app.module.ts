18342f8857a230dd7771fa4a1f337ea8c575d4b1
abdellah <abdellahedaoudi80@gmail.com>
2024-11-15 15:41:54 +0100

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './Users/user.module';
@@ -14,6 +14,9 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes('user');
+      .forRoutes(
+        {path :"user",method:RequestMethod.GET},
+        {path :"user",method:RequestMethod.POST}
+      );
   }
 }
